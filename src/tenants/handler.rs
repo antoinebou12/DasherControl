4b792cd023ee44d7bcbd51913600ee3ac3b4d60d
Antoine Boucher <antoine.bou13@gmail.com>
2021-03-28 19:07:34 -0400

    vuesax 4 dark mode

--- src/tenants/handler.rs
+++ src/tenants/handler.rs
@@ -10,7 +10,7 @@ use crate::tenants::model::AuthTenant;
 use crate::tenants::model::RegisterTenant;
 use crate::tenants::model::Tenant;
 
-#[get("/")]
+#[get("/list")]
 pub fn all_tenants(conn: DbConn) -> Result<Json<Vec<Tenant>>, Status> {
     return all(&conn)
     .map_err(|error| error_status(error))
@@ -26,30 +26,30 @@ fn error_status(error: Error) -> Status {
 
 
 #[post("/json/create", format="application/json", data = "<tenant>")]
-pub fn create_tenant(conn: DbConn, tenant: Json<RegisterTenant>) -> status::Accepted<String> {
+pub fn create_tenant(conn: DbConn, tenant: Json<RegisterTenant>) -> Result<status::Accepted<String>, Status> {
     let register_tenant = match tenant.into_inner().validates(&conn) {
         Ok(register_tenant) => register_tenant,
-        Err(_) => return status::Accepted(Some("fuck".to_string())),
+        Err(_) => return Err(Status::Conflict),
     };
     let tenant_create = match Tenant::create(register_tenant, &conn) {
-        Ok(tenant) => return status::Accepted(Some("yee".to_string())),
-        Err(_) => panic!(),
+        Ok(tenant) => return Ok(status::Accepted(Some("yee".to_string()))),
+        Err(_) => return Err(Status::Conflict),
     };
 
 }
 
 
 #[post("/json/login", format="application/json", data = "<auth_tenant>")]
-pub fn login(conn: DbConn, auth_tenant: Json<AuthTenant>) -> status::Accepted<String> {
+pub fn login(conn: DbConn, auth_tenant: Json<AuthTenant>) -> Result<status::Accepted<String>, Status> {
 
     let tenant = match auth_tenant.login(&conn) {
         Ok(tenant) => tenant,
-        Err(_) => panic!(),
+        Err(_) => return Err(Status::Conflict),
     };
     // This is the jwt token we will send in a cookie.
     let token = create_token(tenant.id, &tenant.email, &tenant.name).unwrap();
 
-    return status::Accepted(Some(format!("token: '{}'", token.to_string())));
+    return Ok(status::Accepted(Some(format!("token: '{}'", token.to_string()))));
 
     // Finally our response will have a csrf token for security. 
     // hex::encode(generator.generate())
