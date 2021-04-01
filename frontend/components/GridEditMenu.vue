99fd51b36e126b565402988c98d4edc2e1575bb1
Antoine Boucher <antoine.bou13@gmail.com>
2021-04-01 11:17:19 -0400

    applet database and rest workspace frontend basic

--- frontend/components/GridEditMenu.vue
+++ frontend/components/GridEditMenu.vue
@@ -4,37 +4,39 @@
     <vs-button @click="gridLayout.removeItem()"><i class="bx bx-x"/></vs-button>
     <vs-button @click="gridLayout.lockGridLayout()"><i class="bx bx-lock-alt"/></vs-button>
     <vs-button @click="gridLayout.unlockGridLayout()"><i class="bx bx-lock-open-alt"/></vs-button>
+    <vs-button @click="saveWorkspaceLayout()"><i class="bx bx-save"/></vs-button>
 <!--    <vs-button @click="gridLayout.saveGridlayout()"><i class="bx bx-save"/></vs-button>-->
 <!--    <vs-sidebar absolute hover-expend reduce v-model="sidebarActive">-->
-<!--      <vs-sidebar-item @click="gridLayout.addItem()">-->
+<!--      <vs-sidebar-this.gridLayout.layout[i] @click="gridLayout.addthis.gridLayout.layout[i]()">-->
 <!--        <template #icon>-->
 <!--          <i class="bx bx-add-to-queue"/>-->
 <!--        </template>-->
 <!--        Add Bloc-->
-<!--      </vs-sidebar-item>-->
-<!--      <vs-sidebar-item @click="gridLayout.removeItem()">-->
+<!--      </vs-sidebar-this.gridLayout.layout[i]>-->
+<!--      <vs-sidebar-this.gridLayout.layout[i] @click="gridLayout.removethis.gridLayout.layout[i]()">-->
 <!--        <template #icon>-->
 <!--          <i class="bx bx-x"/>-->
 <!--        </template>-->
 <!--        Remove Bloc-->
-<!--      </vs-sidebar-item>-->
-<!--      <vs-sidebar-item @click="gridLayout.lockGridLayout()">-->
+<!--      </vs-sidebar-this.gridLayout.layout[i]>-->
+<!--      <vs-sidebar-this.gridLayout.layout[i] @click="gridLayout.lockGridLayout()">-->
 <!--        <template #icon>-->
 <!--          <i class="bx bx-lock-alt"/>-->
 <!--        </template>-->
 <!--        Lock Grid-->
-<!--      </vs-sidebar-item>-->
-<!--      <vs-sidebar-item @click="gridLayout.unlockGridLayout()">-->
+<!--      </vs-sidebar-this.gridLayout.layout[i]>-->
+<!--      <vs-sidebar-this.gridLayout.layout[i] @click="gridLayout.unlockGridLayout()">-->
 <!--        <template #icon>-->
 <!--          <i class="bx bx-lock-open-alt"/>-->
 <!--        </template>-->
 <!--        Unlock Grid-->
-<!--      </vs-sidebar-item>-->
+<!--      </vs-sidebar-this.gridLayout.layout[i]>-->
 <!--    </vs-sidebar>-->
   </div>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
   name: "GridEditMenu",
@@ -43,7 +45,37 @@ export default {
   },
   data: () => ({
     sidebarActive: ''
-  })
+  }),
+  methods: {
+    saveWorkspaceLayout(){
+      let applets_layout = []
+      for (let i=0;i < this.gridLayout.layout.length;i++){
+        let applet = {}
+        applet.name = this.gridLayout.layout[i].i
+        applet.position_x = this.gridLayout.layout[i].x
+        applet.position_y = this.gridLayout.layout[i].y
+        applet.width = this.gridLayout.layout[i].w
+        applet.height = this.gridLayout.layout[i].y
+        applet.editable = this.gridLayout.layout[i].static
+        applet.applet_data = this.gridLayout.layout[i].appletData.toString()
+        applets_layout.push(applet)
+      }
+
+      axios({
+        method: 'post',
+        url: '/workspaces/api/create',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          name: "workspace",
+          display_order: 0,
+          tenant_id: 1,
+          applets: applets_layout
+        }
+      });
+    }
+  }
 }
 </script>
 
--- src/workspaces/handler.rs
+++ src/workspaces/handler.rs
@@ -3,12 +3,12 @@ use rocket::response::status;
 use rocket_contrib::json::Json;
 
 use crate::db::DbConn;
-use crate::workspaces::model::{NewWorkspace, Workspace, NewApplet, Applet};
+use crate::workspaces::model::{NewWorkspace, Workspace, NewApplet, Applet, NewWorkspaceWithApplets};
 use diesel::result::Error;
 
 #[get("/api/applets/<workspace_id>")]
 pub fn get_applets_in_workspace(conn: DbConn, workspace_id: i32) -> Result<Json<Vec<Applet>>, Status> {
-    return Applet::all_applets_by_workspace(workspace_id, &conn)
+    return Applet::all_applets_by_workspace(&conn, workspace_id)
         .map_err(|error| error_status(error))
         .map(|applets| Json(applets));
 }
@@ -21,10 +21,10 @@ fn error_status(error: Error) -> Status {
     }
 }
 
-#[post("/api/create", format="application/json", data = "<workspace>")]
+#[post("/api/new", format="application/json", data = "<workspace>")]
 pub fn create_workspace(conn: DbConn, workspace: Json<NewWorkspace>) -> Result<status::Accepted<String>, Status> {
     let new_workspace = workspace.into_inner();
-    let workspace_create = match Workspace::create(new_workspace, &conn) {
+    let workspace_create = match Workspace::create( &conn, new_workspace,) {
         Ok(workspace) => return Ok(status::Accepted(Some("workspace created".to_string()))),
         Err(_) => return Err(Status::Conflict)
     };
@@ -34,8 +34,18 @@ pub fn create_workspace(conn: DbConn, workspace: Json<NewWorkspace>) -> Result<s
 #[post("/api/applets/create", format="application/json", data = "<applet>")]
 pub fn create_applet(conn: DbConn, applet: Json<NewApplet>) -> Result<status::Accepted<String>, String> {
     let new_applet = applet.into_inner();
-    let applet_create = match Applet::create(new_applet, &conn) {
+    let applet_create = match Applet::create(&conn, new_applet) {
         Ok(applet) => return Ok(status::Accepted(Some("applets created".to_string()))),
         Err(e) => return Err(format!("{:?}", e.to_string()))
     };
+}
+
+#[post("/api/create", format="application/json", data = "<workspace>")]
+pub fn create_workspace_with_applets(conn: DbConn, workspace: Json<NewWorkspaceWithApplets>, ) -> Result<status::Accepted<String>, Status> {
+    let new_workspace = workspace.into_inner();
+    let workspace_create = match Workspace::create_with_applets(&conn, new_workspace) {
+        Ok(workspace) => return Ok(status::Accepted(Some("workspace with applets created".to_string()))),
+        Err(_) => return Err(Status::Conflict)
+    };
+
 }
